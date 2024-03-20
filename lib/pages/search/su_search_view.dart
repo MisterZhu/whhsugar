8857da2c3b761940399d94e9b93f2eb3e5806c77
zhulixiao <1280253718@qq.com>
2024-03-20 18:20:15 +0800

    Pull away Code

--- lib/pages/search/su_search_view.dart
+++ lib/pages/search/su_search_view.dart
@@ -29,42 +29,54 @@ class SUSearchPage extends StatelessWidget {
       child: ListView.builder(
         itemCount: yourData.length, // 分组数
         itemBuilder: (context, index) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                    top: 24.0.w, left: 20.w, right: 20.w, bottom: 10.w),
-                child: Text(
-                  'Group ${index + 1}',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+          return rowItem(index);
+        },
+      ),
+    );
+  }
+
+  Widget rowItem(int index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        rowHeader(index),
+        rowListView(index),
+      ],
+    );
+  }
+
+  Widget rowHeader(int index) {
+    return Padding(
+      padding:
+          EdgeInsets.only(top: 24.0.w, left: 20.w, right: 20.w, bottom: 10.w),
+      child: Text(
+        'Group ${index + 1}',
+        style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: SUColorSingleton().textColor),
+      ),
+    );
+  }
+
+  Widget rowListView(int index) {
+    return Container(
+      height: 200, // 卡片高度
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: yourData[index].length, // 每组中卡片数量
+        itemBuilder: (context, cardIndex) {
+          return Padding(
+            padding: EdgeInsets.only(left: 20.w),
+            child: Card(
+              child: Container(
+                width: 150, // 卡片宽度
+                color: Colors.grey,
+                child: Center(
+                  child: Text(yourData[index][cardIndex]), // 根据数据显示内容
                 ),
               ),
-              Container(
-                height: 200, // 卡片高度
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: yourData[index].length, // 每组中卡片数量
-                  itemBuilder: (context, cardIndex) {
-                    return Padding(
-                      padding: EdgeInsets.only(left: 20.w),
-                      child: Card(
-                        child: Container(
-                          width: 150, // 卡片宽度
-                          color: Colors.grey,
-                          child: Center(
-                            child: Text(yourData[index][cardIndex]), // 根据数据显示内容
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
+            ),
           );
         },
       ),
